e5dba6e6d150967c306b2a5346ea0337737eb172
Kristina Jassova <kristinka.jassova@gmail.com>
2022-05-04 18:51:26 +0200

    GUI - main page (version 1)

---
+++ main/src/test/TestBookingSystemGUI.java
@@ -0,0 +1,15 @@
+package test;
+
+import javafx.application.Application;
+import view.BookingManagementGUI;
+
+public class TestBookingSystemGUI
+{
+  public static void main(String[] args)
+  {
+
+    Application.launch(BookingManagementGUI.class);
+
+
+  }
+}
--- main/src/view/BookingManagementGUI.java
+++ main/src/view/BookingManagementGUI.java
@@ -1,12 +1,25 @@
 package view;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class BookingManagementGUI extends Application
 {
-  public void start(Stage window)
-  {
+  @FXML private PieChart availability;
 
+  public void start(Stage window) throws IOException
+  {
+    window.setTitle("OverLook Hotel Booking Manager");
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(getClass().getResource("mainview.fxml"));
+    Scene scene = new Scene(loader.load());
+    window.setScene(scene);
+    window.show();
   }
 }

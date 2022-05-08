17dc113392f0c29a06be0244646bcb8b2a5bb5a0
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2022-05-08 11:21:35 +0200

    test

--- main/src/model/Room.java
+++ main/src/model/Room.java
@@ -1,5 +1,77 @@
 package model;
 
-public class Room
-{
+/**
+ * Room class.
+ *
+ * @author Dragos Cotaga
+ * @version 1.0
+ * @see Room
+ * @since 1.0
+ */
+
+
+public class Room {
+    private String roomNumber, roomType;
+    private boolean extraBed, available;
+    private double price;
+
+    public Room(String roomNumber, String roomType, boolean extraBed, double price) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.extraBed = extraBed;
+        this.price = price;
+        this.available = true;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public boolean isExtraBed() {
+        return extraBed;
+    }
+
+    public void setExtraBed(boolean extraBed) {
+        this.extraBed = extraBed;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailability(boolean available) {
+        this.available = available;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public GuestList getAllGuest() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Room Number: " + roomNumber + "\nRoom Type: " + roomType + "\nExtra Bed: " + extraBed + "\nPrice: " + price;
+    }
 }
+
+
+
--- main/src/model/RoomList.java
+++ main/src/model/RoomList.java
@@ -1,5 +1,41 @@
 package model;
 
-public class RoomList
-{
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * RoomList class is a list of rooms.
+ *
+ * @author Dragos Cotaga
+ * @version 1.0
+ * @see Room
+ * @see RoomList
+ * @since 1.0
+ */
+
+// Class: RoomList extends ArrayList and implements RoomListInterface and Serializable interfaces to store the list of rooms in the hotel
+public class RoomList {
+    private ArrayList<Room> rooms;
+
+    // Constructor: creates an empty list of rooms
+    public RoomList() {
+        rooms = new ArrayList<Room>();
+    }
+
+    // Method: adds a room to the list of rooms if the room is not already in the list of rooms and returns true if the room is added successfully
+    public void addRoom(Room room) {
+        rooms.add(room);
+    }
+
+    // Method: returns the list of rooms in the hotel as an array of rooms and returns null if the list of rooms is empty
+    public Room getRoom(int index) {
+        return rooms.get(index);
+    }
+
+    @Override
+    public String toString() {
+        return "RoomList{" +
+                "rooms=" + rooms +
+                '}';
+    }
 }

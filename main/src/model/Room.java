fa82ad1a2c6e5e5f85b9e5e048b68581305742d2
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2022-05-10 11:12:17 +0200

    Added comments for Room class.

--- main/src/model/Room.java
+++ main/src/model/Room.java
@@ -4,17 +4,19 @@ package model;
  * Room class.
  *
  * @author Dragos Cotaga
- * @version 1.0
+ * @version 1.1
  * @see Room
- * @since 1.0
+ * @since 2022-05-10
  */
 
-
+// Method: getRoomNumber() - returns the room number of the room object being called on in the method call (Room room = new Room(1); room.getRoomNumber();)
 public class Room {
     private String roomNumber, roomType;
     private boolean extraBed, available;
     private double price;
 
+
+    // Constructor for the Room class. Initializes the room number, room type, extra bed, available, and price variables.
     public Room(String roomNumber, String roomType, boolean extraBed, double price) {
         this.roomNumber = roomNumber;
         this.roomType = roomType;
@@ -23,46 +25,57 @@ public class Room {
         this.available = true;
     }
 
+    // Method: getRoomNumber() - returns the room number of the room object being called on in the method call (Room room = new Room(1); room.getRoomNumber();)
     public String getRoomNumber() {
         return roomNumber;
     }
 
+    // Method: setRoomNumber() - sets the room number of the room object being called on in the method call (Room room = new Room(1); room.setRoomNumber(2);)
     public void setRoomNumber(String roomNumber) {
         this.roomNumber = roomNumber;
     }
 
+    // Method: getRoomType() - returns the room type of the room object being called on in the method call (Room room = new Room(1); room.getRoomType();)
     public String getRoomType() {
         return roomType;
     }
 
+    // Method: setRoomType() - sets the room type of the room object being called on in the method call (Room room = new Room(1); room.setRoomType("Single");)
     public void setRoomType(String roomType) {
         this.roomType = roomType;
     }
 
+    // Method: isExtraBed() - returns the extra bed status of the room object being called on in the method call (Room room = new Room(1); room.isExtraBed();)
     public boolean isExtraBed() {
         return extraBed;
     }
 
+    // Method: setExtraBed() - sets the extra bed status of the room object being called on in the method call (Room room = new Room(1); room.setExtraBed(true);)
     public void setExtraBed(boolean extraBed) {
         this.extraBed = extraBed;
     }
 
+    // Method: isAvailable() - returns true if the room is available, false if it is not.
     public boolean isAvailable() {
         return available;
     }
 
+    // Method: setAvailable() - sets the room to available or not available.
     public void setAvailability(boolean available) {
         this.available = available;
     }
 
+    // Method: getPrice() - returns the price of the room.
     public double getPrice() {
         return price;
     }
 
+    // Method: setPrice() - sets the price of the room.
     public void setPrice(double price) {
         this.price = price;
     }
 
+    // Method: GuestList() - returns the list of guests.
     public GuestList getAllGuest() {
         return null;
     }
